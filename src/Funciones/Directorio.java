f72b517600d078cb99cc41457c0c6074cefe3eda
JuanMFR <jmfrdz98@gmail.com>
2025-09-08 19:44:21 -0300

    Primer commit clases

---
+++ src/Funciones/Directorio.java
@@ -0,0 +1,62 @@
+
+package Funciones;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author JuanmaPC
+ */
+public class Directorio {
+    
+    private TreeMap<Long, Contacto> telefono_contactos = new TreeMap<>();
+    
+    public TreeMap<Long, Contacto> getTelefono_contactos() {
+        return telefono_contactos;
+    }
+    
+    public void agregarContacto(long telefono, Contacto contacto){
+        telefono_contactos.put(telefono, contacto);
+    }
+    
+    public Contacto buscarContacto(long telefono){
+        if(telefono_contactos.containsKey(telefono)){
+            return telefono_contactos.get(telefono);
+        }
+        return null;
+    }
+    
+    public Set<Long> buscarTelefono(String apellido){
+        Set<Long> telefonosEncontrados = new HashSet<>();
+        Iterator<Map.Entry<Long, Contacto>> iter = telefono_contactos.entrySet().iterator();
+        while(iter.hasNext()){
+            Map.Entry<Long, Contacto> entry = iter.next();
+            if(entry.getValue().getApellido().equals(apellido)){
+                telefonosEncontrados.add(entry.getKey());
+            }
+        }
+        return telefonosEncontrados;
+    }
+
+    public ArrayList<Contacto> buscarContactos(String ciudad){
+        ArrayList<Contacto> contactosEncontrados = new ArrayList<>();
+        Iterator<Contacto> iter = telefono_contactos.values().iterator();
+        while (iter.hasNext()){
+            Contacto contacto = iter.next();
+            if(contacto.getCiudad().equals(ciudad)){
+                contactosEncontrados.add(contacto);
+            }
+        }
+        return contactosEncontrados;
+    }
+    
+    public Contacto borrarContacto(long telefono){
+        return telefono_contactos.remove(telefono);
+    }
+    
+}
